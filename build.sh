bd4614a7377bd706a5070b88aa090b530de2c027
Bas81r1t <bas81r1t@gmail.com>
2025-07-28 13:56:30 +0530

    🛠️ Added final working build.sh for Render

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
-python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic --no-input
-python3 manage.py migrate
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files for CSS etc.
+python manage.py collectstatic --noinput
+
+# Apply database migrations
+python manage.py migrate

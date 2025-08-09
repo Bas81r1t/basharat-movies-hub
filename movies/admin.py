fda2829d88e68f8b7d64e5039812148c5f0bf41f
Bas81r1t <bas81r1t@gmail.com>
2025-08-09 11:31:12 +0530

    Removed green download button and hid udrop link from admin panel

--- movies/admin.py
+++ movies/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Playlist, Movie
 
-admin.site.register(Playlist)
-admin.site.register(Movie)
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    exclude = ('udrop_link',)  # ✅ Hide udrop_link from admin panel
 
+@admin.register(Playlist)
+class PlaylistAdmin(admin.ModelAdmin):
+    pass
